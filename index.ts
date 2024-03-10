6a08f9cadaa94aacc2310f522aab44c6c1d54791
BiliksuunSamuel <samuel.biliksuun@hubtel.com>
2024-03-10 22:32:50 +0000

    updated test

--- index.ts
+++ index.ts
@@ -1,3 +1,2 @@
 import { IsValidPhoneNumber } from "./src/utils";
-// console.log(IsValidPhoneNumber("447539278773", "GB"));
-console.log(IsValidPhoneNumber("233550465223", "GH"));
+console.log(IsValidPhoneNumber("23302000000", "GH"));
